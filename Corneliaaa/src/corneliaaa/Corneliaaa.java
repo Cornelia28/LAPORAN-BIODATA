c63bb3ae29294fd2e601f5a6ceaea2c941698531
Cornelia28 <75266999+Cornelia28@users.noreply.github.com>
2021-02-23 16:37:28 +0700

    Add files via upload

---
+++ Corneliaaa/src/corneliaaa/Corneliaaa.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package corneliaaa;
+
+/**
+ *
+ * @author Neli
+ */
+public class Corneliaaa {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String nama="Cornelia Tamaya Fadihilah";
+        String kelas="11 RPL 2";
+        int absen=07;
+        String jurusan="Rekayasa Perangkat Lunak";
+        String alamat="Karangsono Sukorejo";
+        String tempat_lahir="Malang";
+        String tanggal_lahir="28 Januari 2004";
+        
+        System.out.println("Biodata Siswa");
+        System.out.println("Nama \t\t\t\t\ :"+nama);
+        System.out.println("Kelas \t\t\t\t\ :"+kelas);
+        System.out.println("Absen \t\t\t\t\ :"+absen);
+        System.out.println("Jurusan \t\t\t\ :"+jurusan);
+        System.out.println("Alamat \t\t\t\t\ :"+alamat);
+        System.out.println("Tempat Lahir \t\t\t\ :"+tempat_lahir);
+        System.out.println("Tanggal Lahir \t\t\t\ :"+tanggal_lahir);
+                
+        
+    }
+    
+}
